1b429442faa7967f34c01e5f5c3be719041280b6
Clesh <ekokoclesh07@gmail.com>
2020-06-10 16:04:33 +0200

    programa principal para mostrar a hora e data do sistema

---
+++ hora.c
@@ -0,0 +1,9 @@
+﻿#include "hora_do_sistema.h"
+#include <time.h>
+int main(){
+	time_t datah;
+	/*datah variavel que recebe o retorno da funcao data_atual*/
+	datah=data_atual();
+	printf(datah);
+	
+}
---
+++ hora_do_sistema.h
@@ -0,0 +1,7 @@
+﻿#include <time.h>
+int data_atual(void){
+	time_t data,t;
+	t=time(NULL);
+	data=ctime(&t);
+	return data;
+}
